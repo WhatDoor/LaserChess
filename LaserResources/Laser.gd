5707b4e205a744ecc7ee924b6293bc354d6c8f8c
WhatDoor <jacobwongzunyi@hotmail.com>
2020-04-24 19:09:16 +1000

    lifted the projectile off the ground babyyty

--- LaserResources/Laser.gd
+++ LaserResources/Laser.gd
@@ -5,10 +5,13 @@ enum {
 	RIGHT
 }
 
-export var fire_rate = 0.5
-
 var state = RIGHT
+
+export var fire_rate = 0.5
 var can_fire = true
+
+var CENTRE_OFFSET = 12
+var VERTICAL_OFFSET = Vector2(0,-6.7)
 
 var Blast = preload("res://Projectiles/Blast.tscn")
 
@@ -32,12 +35,19 @@ func animate():
 
 func _process(delta):
 	if Input.is_action_pressed("End_Turn") and can_fire:
+		#Instance, rotate and position blast
 		var blast = Blast.instance()
 		blast.position = get_position()
-		blast.rotation_degrees = rotation_degrees
-		var rot = deg2rad(rotation_degrees)
+		blast.rotation_degrees = rotation_degrees - 90
 		
+		var rot = deg2rad(rotation_degrees - 90)
 		blast.fire(Vector2(-sin(rot), cos(rot)))
+		
+		#Offset position
+		#blast.position += blast.velocity * CENTRE_OFFSET
+		#blast.position += VERTICAL_OFFSET
+		
+	
 		get_parent().add_child(blast)
 		
 		can_fire = false
--- Projectiles/Blast.gd
+++ Projectiles/Blast.gd
@@ -5,6 +5,14 @@ var velocity = Vector2.ZERO
 
 func fire(direction):
 	velocity = direction
+	$Sprite.position.x = get_sprite_ground_offset(velocity)
+	
+func get_sprite_ground_offset(direction):
+	#Dot Product to get angle between direction and X axis, result should be between 0 (perpendicular) and 1 (parallel)
+	var angle = direction.dot(Vector2(1,0))
+	
+	#Scale angle to offset 0 is 0 and 1 is 8
+	return angle * 8
 
 func _process(delta):
 	position += velocity.normalized() * speed
